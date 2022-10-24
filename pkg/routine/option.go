8540e472fe66a68d1dd9a80d6543848220459826
crochee <licf137@chinaunicom.cn>
2022-10-24 15:23:40 +0800

    [Feature] 模板同步实现

--- pkg/routine/option.go
+++ pkg/routine/option.go
@@ -4,9 +4,16 @@ import "context"
 
 type option struct {
 	recoverFunc func(ctx context.Context, r interface{})
+	copyContext func(dst context.Context, src context.Context) context.Context
 }
 
+type Option func(*option)
+
 // Recover register to Pool
-func Recover(f func(context.Context, interface{})) func(*option) {
+func Recover(f func(context.Context, interface{})) Option {
 	return func(o *option) { o.recoverFunc = f }
+}
+
+func CopyContext(f func(dst context.Context, src context.Context) context.Context) Option {
+	return func(o *option) { o.copyContext = f }
 }
