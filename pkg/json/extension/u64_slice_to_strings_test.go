bd13d1fa046fa027fac90d259da65409ddf5f279
lukefu <1506958820@qq.com>
2023-04-18 20:43:07 +0800

    fix: httpclient 和 json 功能补充 1、json 增加字符串数组转 整型数据功能 2、http 工具 param函数和setheader 增加 字符串为空时过滤功能

--- pkg/json/extension/u64_slice_to_strings_test.go
+++ pkg/json/extension/u64_slice_to_strings_test.go
@@ -1,6 +1,7 @@
 package extension
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -57,6 +58,60 @@ func Test_U64s_Encode(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Marshal() got = %s, want %s", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_U64s_Decode(t *testing.T) {
+	type TestString struct {
+		Foo []uint64 `json:"foo,strings"`
+		Bar []uint64 `json:"bar"`
+	}
+
+	tests := []struct {
+		name    string
+		input   []byte
+		want    TestString
+		wantErr bool
+	}{
+		{
+			name:    "none",
+			input:   []byte(`{"foo":[],"bar":null}`),
+			want:    TestString{},
+			wantErr: false,
+		},
+		{
+			name: "normal",
+			want: TestString{
+				Foo: []uint64{330120267575640153, 330120472828100697, 331857912358027388},
+				Bar: []uint64{330120267575640153, 330120472828100697, 331857912358027388},
+			},
+			input:   []byte(`{"foo":["330120267575640153","330120472828100697","331857912358027388"],"bar":[330120267575640153,330120472828100697,331857912358027388]}`),
+			wantErr: false,
+		},
+		{
+			name: "oneItem",
+			want: TestString{
+				Foo: []uint64{330120267575640153},
+				Bar: []uint64{330120267575640153, 330120472828100697, 331857912358027388},
+			},
+			input:   []byte(`{"foo":["330120267575640153"],"bar":[330120267575640153,330120472828100697,331857912358027388]}`),
+			wantErr: false,
+		},
+	}
+
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	json.RegisterExtension(&u64SliceAsStringsCodec{})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got TestString
+			err := json.Unmarshal(tt.input, &got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Marshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println("got:", got)
+			fmt.Println("want", tt.want)
 		})
 	}
 }
