cbbb01ff6cfbf2c82610dd1bd4a28654d7cc8ad6
crochee <licf137@chinaunicom.cn>
2023-09-05 11:21:59 +0800

    [Feature] 增加函数job的封装

--- pkg/job/job.go
+++ pkg/job/job.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"context"
+	"reflect"
 )
 
 // Job represents an interface to be implemented by structs which represent a 'job'
@@ -15,4 +16,28 @@ type Job interface {
 
 	// Execute is called by a SchedulerRuntime when the Trigger associated with this job fires.
 	Execute(context.Context)
+}
+
+type funcJob struct {
+	key string
+	f   func(context.Context)
+}
+
+func (f *funcJob) Description() string {
+	return f.key
+}
+
+func (f *funcJob) Key() string {
+	return f.key
+}
+
+func (f *funcJob) Execute(ctx context.Context) {
+	f.f(ctx)
+}
+
+func NewFuncJob(f func(context.Context)) Job {
+	return &funcJob{
+		key: reflect.TypeOf(f).String(),
+		f:   f,
+	}
 }
