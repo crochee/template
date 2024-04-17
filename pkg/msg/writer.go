2c5939f6ea1947377fe8a4bf34bece685a92cf05
crochee <13522570308@139.com>
2024-04-17 15:45:07 +0800

    feat: 增加父子节点关联保留逻辑

--- pkg/msg/writer.go
+++ pkg/msg/writer.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"github.com/ThreeDotsLabs/watermill/message"
-	uuid "github.com/gofrs/uuid"
 	jsoniter "github.com/json-iterator/go"
+	uuid "github.com/satori/go.uuid"
 	"github.com/streadway/amqp"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
@@ -75,11 +75,12 @@ func (w *Writer) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan)
 		return nil
 	}
 	result := make([]amqp.Publishing, 0, len(spans))
-	traceIDs := make([]string, 0, len(spans))
+	keeps := make([]*Metadata, 0, len(spans))
+	paredntNeedSends := make(map[string]struct{}, len(spans)/2)
 	for i := range spans {
 		metadata := w.MetadataPool.Get()
+		metadata.TraceID = "req-" + uuid.UUID(spans[i].SpanContext().TraceID()).String()
 		events := spans[i].Events()
-		// tempEvents := make([]sdktrace.Event, 0, len(events))
 		tempEvents := &DescContent{}
 		// 去重map初始化
 		oreqMap := make(map[string]Event)
@@ -128,20 +129,7 @@ func (w *Writer) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan)
 			default:
 			}
 		}
-		if len(tempEvents.List) == 0 {
-			w.MetadataPool.Put(metadata)
-			continue
-		}
-		for _, httpInfo := range oreqMap {
-			tempEvents.List = append(tempEvents.List, httpInfo)
-		}
-		data, err := w.JSONHandler.Marshal(tempEvents)
-		if err != nil {
-			w.From(ctx).Errorf("marshal events failed,%+v", err)
-			continue
-		}
-		metadata.Desc = string(data)
-
+		var keep bool
 		for _, attr := range spans[i].Attributes() {
 			switch attr.Key {
 			case LocateKey:
@@ -158,29 +146,34 @@ func (w *Writer) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan)
 				metadata.SubResID = attr.Value.AsString()
 			case SubResTypeKey:
 				metadata.SubResType = attr.Value.AsString()
+			case KeepKey:
+				keep = attr.Value.AsBool()
 			default:
 			}
 		}
-		metadata.TraceID = "req-" + uuid.UUID(spans[i].SpanContext().TraceID()).String()
-		traceIDs = append(traceIDs, metadata.TraceID)
-		metadata.ServiceName = w.ServiceNameFunc()
-		metadata.SpanID = spans[i].SpanContext().SpanID().String()
-
-		if spans[i].Parent().HasSpanID() {
-			metadata.ParentSpanID = spans[i].Parent().SpanID().String()
-		}
-		data, err = w.JSONHandler.Marshal(metadata)
-		if err != nil {
-			w.From(ctx).Errorf("Publish failed,%+v,trace_id:%s", err, metadata.TraceID)
+		// 只要有错误事件的，直接标记为发送
+		if len(tempEvents.List) != 0 {
+			w.fillMetadata(ctx, metadata, oreqMap, tempEvents, spans[i])
+			// 记录可以发送的父span
+			if metadata.ParentSpanID != "" {
+				paredntNeedSends[metadata.ParentSpanID] = struct{}{}
+			}
+			result = w.appendResult(ctx, result, metadata)
 			continue
 		}
-		msg, err := w.marshal.Marshal(message.NewMessage(metadata.TraceID, data))
-		if err != nil {
-			w.From(ctx).Errorf("marshal failed,%+v,trace_id:%s", err, metadata.TraceID)
+		// 没有错误事件的，如果需要保留的，将他记录在keeps中，否则丢弃
+		if keep {
+			w.fillMetadata(ctx, metadata, oreqMap, tempEvents, spans[i])
+			keeps = append(keeps, metadata)
 			continue
 		}
-		result = append(result, msg)
 		w.MetadataPool.Put(metadata)
+	}
+	// 遍历需保留的数据是否是已确定发送的数据的父span内容
+	for _, metadata := range keeps {
+		if _, ok := paredntNeedSends[metadata.SpanID]; ok {
+			result = w.appendResult(ctx, result, metadata)
+		}
 	}
 	if len(result) == 0 {
 		return nil
@@ -188,8 +181,51 @@ func (w *Writer) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan)
 	if err := w.GetPublisher().Publish(ctx, w.GetChannel(), w.Cfg.Exchange(), w.Cfg.RoutingKey(), result); err != nil {
 		w.From(ctx).Errorf("Publish failed,%+v", err)
 	}
-	w.From(ctx).Infof("trace_ids:%+v publish success", traceIDs)
 	return nil
+}
+
+func (w *Writer) fillMetadata(
+	ctx context.Context,
+	metadata *Metadata,
+	oreqMap map[string]Event,
+	tempEvents *DescContent,
+	span sdktrace.ReadOnlySpan,
+) {
+	for _, httpInfo := range oreqMap {
+		tempEvents.List = append(tempEvents.List, httpInfo)
+	}
+	data, err := w.JSONHandler.Marshal(tempEvents)
+	if err != nil {
+		w.From(ctx).Errorf("marshal events failed,%+v", err)
+		return
+	}
+	metadata.Desc = string(data)
+	metadata.ServiceName = w.ServiceNameFunc()
+	metadata.SpanID = span.SpanContext().SpanID().String()
+
+	if span.Parent().HasSpanID() {
+		metadata.ParentSpanID = span.Parent().SpanID().String()
+	}
+}
+
+func (w *Writer) appendResult(
+	ctx context.Context,
+	result []amqp.Publishing,
+	metadata *Metadata,
+) []amqp.Publishing {
+	data, err := w.JSONHandler.Marshal(metadata)
+	if err != nil {
+		w.From(ctx).Errorf("Publish failed,trace_id:%s,err:%+v", metadata.TraceID, err)
+		return result
+	}
+	msg, err := w.marshal.Marshal(message.NewMessage(metadata.TraceID, data))
+	if err != nil {
+		w.From(ctx).Errorf("marshal failed,trace_id:%s,err:%+v", metadata.TraceID, err)
+		return result
+	}
+	result = append(result, msg)
+	w.MetadataPool.Put(metadata)
+	return result
 }
 
 func (w *Writer) Shutdown(ctx context.Context) error {
