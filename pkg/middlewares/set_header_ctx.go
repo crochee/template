17f2b76feec2fb843fb25352a3f41d58d67706a2
panxz8 <2235654016@qq.com>
2024-09-14 16:45:44 +0800

    补充trace_id处理（原本cp到了woslo, 那块已经弃用）

--- pkg/middlewares/set_header_ctx.go
+++ pkg/middlewares/set_header_ctx.go
@@ -1,12 +1,11 @@
 package middlewares
 
 import (
-	"errors"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
 
-	"template/pkg/resp"
 	"template/pkg/utils/v"
 )
 
@@ -17,14 +16,15 @@ func SetHeaderContext(setCtx func(*gin.Context)) func(c *gin.Context) {
 		traceID := c.GetHeader(v.HeaderTraceID)
 		if traceID == "" {
 			traceID = uuid.NewV4().String()
-			c.Request.Header.Set(v.HeaderTraceID, traceID) // 请求头
 		} else {
+			// 检测trace_id是否时uuid,若不是则根据规则生成新的traceID
 			_, err := uuid.FromString(traceID)
 			if err != nil {
-				resp.ErrorParam(c, errors.New("X-Trace-ID is invalid,it must be uuid format"))
-				return
+				traceID = GenerateNewTraceID(traceID)
 			}
 		}
+		c.Request.Header.Set(v.HeaderTraceID, traceID) // 请求头
+
 		// 设置响应头
 		if c.Writer.Header().Get(v.HeaderTraceID) == "" {
 			c.Writer.Header().Set(v.HeaderTraceID, traceID) // 响应头
@@ -32,4 +32,22 @@ func SetHeaderContext(setCtx func(*gin.Context)) func(c *gin.Context) {
 		setCtx(c)
 		c.Next()
 	}
+}
+
+// GenerateNewTraceID 生成合法的trace_id, 有以下规则
+// 1，去除第一个-前面的内容（包括-），如果剩余部分为合法UUID，则使用; 如果不包含-，则重新生成一个新的UUID
+// 2，如果都不合法，则重新生成一个新的UUID
+func GenerateNewTraceID(illegalTraceID string) string {
+	// 获取第一个横杠的位置
+	index := strings.Index(illegalTraceID, "-")
+	if index == -1 {
+		return uuid.NewV4().String()
+	}
+	newTraceID := illegalTraceID[index+1:]
+	// 判断是否合法, 不合法则重新生成一个新的UUID
+	_, err := uuid.FromString(newTraceID)
+	if err != nil {
+		return uuid.NewV4().String()
+	}
+	return newTraceID
 }
